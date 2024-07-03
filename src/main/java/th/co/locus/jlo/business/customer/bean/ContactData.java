e9e691295fdc268d247cbe4e4a9147a0ef1c45d4
apichat <apichathot@gmail.com>
2024-07-03 12:59:14 +0700

    contact bean

--- src/main/java/th/co/locus/jlo/business/customer/bean/ContactData.java
+++ src/main/java/th/co/locus/jlo/business/customer/bean/ContactData.java
@@ -3,14 +3,20 @@ package th.co.locus.jlo.business.customer.bean;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import th.co.locus.jlo.common.BaseModelBean;
-
+/***
+ * @author Apichat
+ */
 @Data
 @EqualsAndHashCode(callSuper = true)
 public class ContactData extends BaseModelBean {
 	private int contId;
 	private int custContId;
 	private Long customerId;
+	private String title;
+	private String lastName;
 	private String relationCd;
+	private String phoneNo;
+	private String email;
  
  
 	
