1983551df2be9bf52648e5d289885646a8c6f137
pornarong <pornarong@gmail.com>
2024-07-03 12:51:36 +0700

    update pom.xml to use jlo-start-parent as parent

--- src/main/java/th/co/locus/jlo/kb/KbController.java
+++ src/main/java/th/co/locus/jlo/kb/KbController.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
@@ -19,7 +20,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import io.micrometer.core.instrument.util.StringUtils;
 import th.co.locus.jlo.common.bean.ApiPageResponse;
 import th.co.locus.jlo.common.bean.ApiRequest;
 import th.co.locus.jlo.common.bean.ApiResponse;
